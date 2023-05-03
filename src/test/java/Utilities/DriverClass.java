47da99b4b17426491ee8423d1f4efafb6f0bb88e
berkg <berkguzeyer@yahoo.com>
2023-05-02 20:28:31 -0400

    Arguments added for Chrome (for Jenkins).

--- src/test/java/Utilities/DriverClass.java
+++ src/test/java/Utilities/DriverClass.java
@@ -33,6 +33,7 @@ public class DriverClass {
                 default:
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--remote-allow-origins=*");
+                    options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1400,2400");
                     threadDriver.set(new ChromeDriver(options));
             }
         }
